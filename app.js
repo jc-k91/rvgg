0a2cb621e949c3fadccb4ca4871598f7ffd2f79b
Ju Josh Kim <jktpk91@gmail.com>
2021-04-09 14:52:51 -0700

    set up RAWG API to pull data for games

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log($);
+$(() => { // WINDOW ONLOAD START
+    // fetch(`https://id.twitch.tv/oauth2/token?client_id=uknscyf4gpf19volzwhlxwx6b4mdv2&client_secret=ufxl89hiw4rs2scntqc2z65zktcdf8&grant_type=client_credentials`).then(console.log(data))
+    const keyRAWG = 'e5b52324368b42a2b8079aae967d4128'
+
+    $.ajax({ // AJAX CALL START
+
+        url:'https://api.rawg.io/api/platforms?key=e5b52324368b42a2b8079aae967d4128'
+    }).then(
+        (data)=>{
+            console.log(data)
+        },
+        ()=>{
+            console.log('bad request')
+        }
+    ) // AJAX CALL END
+
+}) // WINDOW ONLOAD END
