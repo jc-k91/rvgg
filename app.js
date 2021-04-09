34a6630fefa7f4ae6432e965a645f19c368d8a0e
Ju Josh Kim <jktpk91@gmail.com>
2021-04-09 14:55:56 -0700

    reformatted API key as variable

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 $(() => { // WINDOW ONLOAD START
-    // fetch(`https://id.twitch.tv/oauth2/token?client_id=uknscyf4gpf19volzwhlxwx6b4mdv2&client_secret=ufxl89hiw4rs2scntqc2z65zktcdf8&grant_type=client_credentials`).then(console.log(data))
+// API key for RAWG; must include with every request
     const keyRAWG = 'e5b52324368b42a2b8079aae967d4128'
-
     $.ajax({ // AJAX CALL START
-
-        url:'https://api.rawg.io/api/platforms?key=e5b52324368b42a2b8079aae967d4128'
+        url:`https://api.rawg.io/api/platforms?key=${keyRAWG}`
     }).then(
         (data)=>{
             console.log(data)
