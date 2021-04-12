e44c46ed5cf41aaa4c0e53f31bfb605c239567b1
Ju Josh Kim <jktpk91@gmail.com>
2021-04-12 09:15:24 -0700

    made a lot of progress on content filters

--- js/app.js
+++ js/app.js
@@ -3,16 +3,14 @@ $(() => { // WINDOW ONLOAD START
     // API key for RAWG; must include with every request
     const keyRAWG = 'e5b52324368b42a2b8079aae967d4128'
     $.ajax({ // AJAX CALL START
-        url:`https://api.rawg.io/api/games?key=${keyRAWG}`,
-        data: {
-            'page-size': 12,
-            'parent-platforms': 'sega',
-            // 'dates': ,
-        }
-
+        url:`https://api.rawg.io/api/games?key=${keyRAWG}&dates=1970-01-01,1999-12-31&parent_platforms=14`,
+        // data: {
+        //     '$limit': 12,
+        // },
     }).then(
         (data)=>{
             console.log(data)
+
         },
         ()=>{
             console.log('bad request')
