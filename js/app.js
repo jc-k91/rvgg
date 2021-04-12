0c11950de296924e41c17fabc48495913e390060
Ju Josh Kim <jktpk91@gmail.com>
2021-04-12 16:07:07 -0700

    almost done with filter menu functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(() => { // WINDOW ONLOAD START
     // API key for RAWG; must include with every request
     const keyRAWG = 'e5b52324368b42a2b8079aae967d4128'
     $.ajax({ // AJAX CALL START
-        url:`https://api.rawg.io/api/games?key=${keyRAWG}&dates=1970-01-01,1999-12-31&parent_platforms=14`,
+        url:`https://api.rawg.io/api/games?key=${keyRAWG}&page_size=24&page=4&dates=1970-01-01,1999-12-31&parent_platforms=12`,
     }).then(
         (data)=>{
             console.log(data)
@@ -16,17 +16,39 @@ $(() => { // WINDOW ONLOAD START
 
     // FUNCTIONS
     // Toggle menu visibility
-    const toggleMenu = () => {
+    const toggleMenu = (event) => {
+        event.preventDefault()
         $('#menu-container').toggleClass('hide')
     }
 
+    // Renders API request results into cards
+    const renderCards = () => {
+
+    }
+
     // LISTENERS
-    // Filter menu modal toggle
+    // Toggles filter menu modal visibility
     $('#menu-button').on('click', toggleMenu)
 
-    // Filter menu close button
+    // Close filter menu on close button click
     $('#close-filters').on('click', toggleMenu)
 
-    // Apply selected filters
-    $('#apply-filters')
+    // Clears filter checkboxes on clear button click
+    $('#clear-filters').on('click', (event) => {
+        event.preventDefault()
+        // console.log($('input[type="checkbox"]').prop('checked'));
+        $('input[type="checkbox"]').prop('checked', false)
+    })
+
+    // // Apply selected filters
+    // $('#apply-filters').on('click', (event) => {
+    //     event.preventDefault()
+    //     $('input[type="checkbox"]').prop(() => {
+    //         this.checked = false
+    //     })
+    // })
+
+
+
+
 }) // WINDOW ONLOAD END
