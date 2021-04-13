db5320ca4484fcd3a2705cca85e6cec1622dd83d
Ju Josh Kim <jktpk91@gmail.com>
2021-04-12 17:43:40 -0700

    created function to parse checked filters into values that can be inserted into API request and made a few adjustments to HTML for compatibility with parser

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(() => { // WINDOW ONLOAD START
     // API key for RAWG; must include with every request
     const keyRAWG = 'e5b52324368b42a2b8079aae967d4128'
     $.ajax({ // AJAX CALL START
-        url:`https://api.rawg.io/api/games?key=${keyRAWG}&page_size=24&page=4&dates=1970-01-01,1999-12-31&parent_platforms=12`,
+        url:`https://api.rawg.io/api/games?key=${keyRAWG}&page_size=24&page=4&dates=1983-01-01,1983-12-31,2016-01-01,2016-12-31`,//&parent_platforms=12`,
     }).then(
         (data)=>{
             console.log(data)
@@ -14,11 +14,44 @@ $(() => { // WINDOW ONLOAD START
         }
     ) // AJAX CALL END
 
-    // FUNCTIONS
+
+//////// GLOBAL VARIABLES ////////
+    // Used in parseFilters() to hold filter values to be used in API request
+    let parsedPlatformFilterArr = []
+    let parsedDateFilterArr = []
+
+
+//////// FUNCTIONS ////////
     // Toggle menu visibility
     const toggleMenu = (event) => {
         event.preventDefault()
         $('#menu-container').toggleClass('hide')
+    }
+
+    // Checks if checkbox is checked (that's a lot of 'check' in one sentence)
+    const checkboxCheckChecker = (filterArrIndex) => {
+        filterArrIndex.checked == true
+    }
+
+    // Parses filter selections into values we can push into API request parameters
+    const parseFilters = () => {
+        parsedPlatformFilterArr = []
+        let selectedFilters = $('input[type="checkbox"]')
+        for (let i = 0; i < 6; i++) {//filterItem of selectedFilters) {
+            console.log($(selectedFilters.eq(i)).attr('checked'));
+            if ($(selectedFilters.eq(i)).attr('checked') == 'checked') {
+                parsedPlatformFilterArr.push($(selectedFilters.eq(i)).val())
+            }
+        }
+        parsedDateFilterArr = []
+        for (let i = 6; i < 8; i++) {//filterItem of selectedFilters) {
+            console.log($(selectedFilters.eq(i)).attr('checked'));
+            if ($(selectedFilters.eq(i)).attr('checked') == 'checked') {
+                parsedDateFilterArr.push($(selectedFilters.eq(i)).val())
+            }
+        }
+        console.log(parsedPlatformFilterArr);
+        console.log(parsedDateFilterArr);
     }
 
     // Renders API request results into cards
@@ -26,9 +59,14 @@ $(() => { // WINDOW ONLOAD START
 
     }
 
-    // LISTENERS
+//////// LISTENERS ////////
     // Toggles filter menu modal visibility
     $('#menu-button').on('click', toggleMenu)
+
+    // Toggles checked attribute on checkbox click
+    $('input[type="checkbox"]').on('click', (event) => {
+        $(event.target).attr('checked', !$(event.target).attr('checked'))
+    })
 
     // Close filter menu on close button click
     $('#close-filters').on('click', toggleMenu)
@@ -40,13 +78,13 @@ $(() => { // WINDOW ONLOAD START
         $('input[type="checkbox"]').prop('checked', false)
     })
 
-    // // Apply selected filters
-    // $('#apply-filters').on('click', (event) => {
-    //     event.preventDefault()
-    //     $('input[type="checkbox"]').prop(() => {
-    //         this.checked = false
-    //     })
-    // })
+    // Applies selected filters on apply button click
+    $('#apply-filters').on('click', (event) => {
+        event.preventDefault()
+        parseFilters()
+        // console.log($('input[type="checkbox"]').toArray());
+        // console.log($('input[type="checkbox"]').prop('checked'));
+    })
 
 
 
